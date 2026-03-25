e42454728df9f5d306c667bc1a0491193ed6b7ba
Souheil Chelfouh <trollfot@gmail.com>
2026-03-25 17:42:24 +0100

    Readying release

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="jsonschema_ui",
-    version="0.1.0",
-    description="JSONSchema UI extraction",
-    author="Christian Klinger",
-    author_email="ck@novareto.de",
-    python_requires=">=3.11",
-    install_requires=["pydantic >= 2", "colander", "deform"],
-    extras_require={
-        "test": [
-            "pytest",
-            "PyHamcrest",
-            "jsonschema_colander",
-        ],
-    },
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.11",
-    ],
-)
+setup()
